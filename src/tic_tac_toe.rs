8cf2867606673c39948134841bd4aad7a30144a7
petereon <pvyboch1@gmail.com>
2023-01-29 22:27:13 +0100

    feat: player creating method

--- src/tic_tac_toe.rs
+++ src/tic_tac_toe.rs
@@ -1,8 +1,12 @@
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct Player {
-    mark: char,
+    pub mark: char,
 }
 
-pub fn create_game(size: u8) -> Vec<Option<Player>> {
+pub fn create_board(size: u8) -> Vec<Option<Player>> {
     return vec![None; (size * size).into()];
+}
+
+pub fn create_player(mark: char) -> Player {
+    return Player { mark: mark };
 }
--- tests/test_tic_tac_toe.rs
+++ tests/test_tic_tac_toe.rs
@@ -1,12 +1,17 @@
 #[cfg(test)]
 mod tic_tac_toe_test {
-    use tic_rust_toe::tic_tac_toe::create_game;
+    use tic_rust_toe::tic_tac_toe::{self, Player};
 
     #[test]
-    fn test_create_game() {
+    fn test_create_board() {
         assert_eq!(
-            create_game(3),
+            tic_tac_toe::create_board(3),
             [None, None, None, None, None, None, None, None, None]
         )
+    }
+
+    #[test]
+    fn test_create_player() {
+        assert_eq!(tic_tac_toe::create_player('X'), Player { mark: 'X' });
     }
 }
